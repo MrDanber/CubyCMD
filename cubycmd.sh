0d24921386ebbdd9bfef65c80fdd0517850ab9e0
MrDanber <Daniel.r.berger@gmail.com>
2018-08-18 21:27:40 +0200

    Update cubycmd.sh

--- cubycmd.sh
+++ cubycmd.sh
@@ -2,3 +2,12 @@
 # Ask the user for their name
 echo Hello how can I help?
 read input 
+
+case "$input" in
+   "apple") echo "Apple pie is quite tasty." 
+   ;;
+   "banana") echo "I like banana nut bread." 
+   ;;
+   "kiwi") echo "New Zealand is famous for kiwi." 
+   ;;
+esac
